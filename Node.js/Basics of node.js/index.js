e8c0064080e30ed3bf33fa884f9d1ea37cf79d75
Yash Yerunkar <yashyerunkar8@gmail.com>
2023-02-07 13:47:59 +0530

    Update index.js

--- Node.js/Basics of node.js/index.js
+++ Node.js/Basics of node.js/index.js
@@ -1,17 +1,34 @@
 const express = require("express");
 
+const books = require("./products.js");
+
 const app = express();
 
 const port = 3001;
 
-app.get("/",(req,res)=>{
-    res.send("using express");
+app.get("/", (req, res) => {
+  res.send("using express");
 });
 
-app.get("/about",(req,res)=>{
-    res.send("about page");
+app.get("/about", (req, res) => {
+  res.send("about page");
 });
 
-app.listen(port,()=>{
-    console.log("listening on port 3000");
-})
+app.get("/books", (req, res) => {
+  res.send(books);
+});
+
+app.get("/books/:name", allBooks, (req, res) => {
+  const name = req.params.name;
+  res.send(name);
+});
+
+function allBooks(req, res, next) {
+  console.log("Fetching all books");
+  console.log((req.params.name = "yash"));
+  next();
+}
+
+app.listen(port, () => {
+  console.log("listening on port 3000");
+});
