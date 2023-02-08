49c4cc5f37d113a58cfdf31ea5101212976696ec
Yash Yerunkar <yashyerunkar8@gmail.com>
2023-02-08 13:08:21 +0530

    style

---
+++ React/React-Tailwind Practice/react-tailwind-1/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold underline text-purple-600 my-2">
+        Hello world
+      </h1>
+    </div>
+  );
+}
+
+export default App;
