2fd2eaf9f0da1a60b41358e9cc20582f110cd001
Yash Yerunkar <yashyerunkar8@gmail.com>
2023-02-20 15:45:34 +0530

    feat: add initial setup

---
+++ React/filmpire/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { CssBaseline } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+      <h1>Filmpire App</h1>
+    </div>
+  );
+}
+
+export default App;
