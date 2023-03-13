1ab36bca1291f33d77627617d3ecfd0580d5ff95
Yash Yerunkar <yashyerunkar8@gmail.com>
2023-03-13 18:59:10 +0530

    feat: basics of angular

---
+++ Angular/angular-first-app/src/app/add-username/add-username.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-username',
+  templateUrl: './add-username.component.html',
+  styleUrls: ['./add-username.component.css']
+})
+export class AddUsernameComponent {
+
+  username = "";
+  
+}
--- Angular/angular-first-app/src/app/app.module.ts
+++ Angular/angular-first-app/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { FormsModule } from '@angular/forms';
+import { AddUsernameComponent } from './add-username/add-username.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    AddUsernameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
