055fa98449aa16d00e026c1994342107a56c3acc
Yash Yerunkar <yashyerunkar8@gmail.com>
2023-03-13 21:46:53 +0530

    feat: add recipe

--- Angular/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+
+  recipes: Recipe[] = [
+    new Recipe("Sample Recipe 1", "descripion 1", "https://media.istockphoto.com/id/1196705449/photo/wooden-vintage-rabbit-a-wooden-vintage-rabbit-toy-is-sitting-on-the-old-books-with-pastel.jpg?s=612x612&w=0&k=20&c=yUZwlPMKbDLTNCM2yifwamZN6LMzrumwSnaBQiQJRLU=")
+  ]
 
 }
