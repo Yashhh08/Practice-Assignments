aa9cf9ccf43fccd5018a2dfb32669b2ade981e6b
Yash Yerunkar <yashyerunkar8@gmail.com>
2023-03-14 23:01:46 +0530

    feat: add recipe details

--- Angular/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Recipe } from './../recipe.model';
 
 @Component({
@@ -9,7 +9,14 @@ import { Recipe } from './../recipe.model';
 export class RecipeListComponent {
 
   recipes: Recipe[] = [
-    new Recipe("Sample Recipe 1", "descripion 1", "https://media.istockphoto.com/id/1196705449/photo/wooden-vintage-rabbit-a-wooden-vintage-rabbit-toy-is-sitting-on-the-old-books-with-pastel.jpg?s=612x612&w=0&k=20&c=yUZwlPMKbDLTNCM2yifwamZN6LMzrumwSnaBQiQJRLU=")
+    new Recipe("Sample Recipe 1", "descripion 1", "https://media.istockphoto.com/id/1196705449/photo/wooden-vintage-rabbit-a-wooden-vintage-rabbit-toy-is-sitting-on-the-old-books-with-pastel.jpg?s=612x612&w=0&k=20&c=yUZwlPMKbDLTNCM2yifwamZN6LMzrumwSnaBQiQJRLU="),
+    new Recipe("Sample Recipe 2", "descripion 2", "https://media.istockphoto.com/id/1196705449/photo/wooden-vintage-rabbit-a-wooden-vintage-rabbit-toy-is-sitting-on-the-old-books-with-pastel.jpg?s=612x612&w=0&k=20&c=yUZwlPMKbDLTNCM2yifwamZN6LMzrumwSnaBQiQJRLU=")
   ]
+
+  @Output() currentRecipeSeleted = new EventEmitter<Recipe>();
+
+  onRecipeSeleted(recipe) {
+    this.currentRecipeSeleted.emit(recipe);
+  }
 
 }
