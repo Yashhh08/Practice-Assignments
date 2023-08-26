3dea41d04523fb3da25c30dd48187035357da2d6
Yash Yerunkar <yashyerunkar8@gmail.com>
2023-08-26 11:53:07 +0530

    feat:

--- React/React Udemy Course/react_basics/src/components/Expense.jsx
+++ React/React Udemy Course/react_basics/src/components/Expense.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expense.css";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
 
 const Expense = (props) => {
+  const [selectedYear, setSelectedYear] = useState("2022");
+
+  const changeFilterHandler = (FilteredYear) => {
+    setSelectedYear(FilteredYear);
+  };
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === selectedYear;
+  });
+
   return (
     <div className="expenses">
-      {props.expenses.map((expense) => {
+      <ExpensesFilter
+        year={selectedYear}
+        onChangeFilter={changeFilterHandler}
+      />
+
+      <ExpensesChart expenses={filteredExpenses} />
+
+      {filteredExpenses.length === 0 && (
+        <p className="noExpenses">No Expenses Found.</p>
+      )}
+
+      {filteredExpenses.map((expense) => {
         return (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
