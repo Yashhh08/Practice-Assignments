9bf8aef2b4ebcd8bead70377f6331c43d395d1f0
Yash Yerunkar <yashyerunkar8@gmail.com>
2023-09-01 20:25:03 +0530

    feat:

---
+++ React/React Udemy Course/react-meals/src/components/Layout/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import MealImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = () => {
+  return (
+    <div>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={MealImage} alt="Food" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
